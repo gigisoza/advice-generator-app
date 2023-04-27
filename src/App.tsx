a0d2caa2e746bffe9c660ba974674982812e3694
gigisoza <sozashviligigi@gmail.com>
2023-04-27 22:54:30 +0400

    Add functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { AdviceCard } from "./components/AdviceCard";
+import { useEffect, useState } from "react";
+import { Advice, AdviceCard } from "./components/AdviceCard";
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState({} as Advice);
+
+  const getAdvice = async () => {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    const advice = await response.data.slip;
+    setAdvice(advice);
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
     <div className="App">
-      <AdviceCard />
+      <AdviceCard advice={advice} getAdvice={getAdvice} />
     </div>
   );
 }
--- src/components/AdviceCard.tsx
+++ src/components/AdviceCard.tsx
@@ -2,15 +2,27 @@ import dividierDesktop from "../images/pattern-divider-desktop.svg";
 import dividerMobile from "../images/pattern-divider-mobile.svg";
 import dice from "../images/icon-dice.svg";
 
-export const AdviceCard = () => {
+export type Advice = {
+  id: number;
+  advice: string;
+};
+
+interface Props {
+  advice: Advice | undefined;
+  getAdvice: () => Promise<void>;
+}
+
+export const AdviceCard = (props: Props) => {
+  const { advice, getAdvice } = props;
+
   return (
     <div className="card">
-      <p>advice #1</p>
-      <h2>This is an advice</h2>
+      <p>advice #{advice?.id}</p>
+      <h2>"{advice?.advice}"</h2>
       <img src={dividierDesktop} className="divider-desktop" alt="divider" />
       <img src={dividerMobile} className="divider-mobile" alt="divider" />
 
-      <div className="dice">
+      <div className="dice" onClick={getAdvice}>
         <img src={dice} alt="dice" />
       </div>
     </div>
